9a087299526d04e0b39d103ba6f27275c54d7691
Aaron McKie <aaron.mckie@icloud.com>
2021-06-14 23:04:43 +0100

    MVP Complete

---
+++ src/main/java/attractions/RollerCoaster.java
@@ -0,0 +1,31 @@
+
+package attractions;
+
+import behaviours.ISecurity;
+import behaviours.ITicketed;
+import people.Visitor;
+
+public class RollerCoaster  extends Attraction implements ISecurity, ITicketed {
+
+    public RollerCoaster(String name, int rating) {
+        super(name, rating);
+    }
+
+    public boolean isAllowed(Visitor visitor) {
+        if(visitor.getHeight() < 145){
+            return false;
+        }
+        return true;
+    }
+
+    public double defaultPrice() {
+        return 8.40;
+    }
+
+    public double priceForVisitor(Visitor visitor) {
+        if(visitor.getHeight() > 200){
+            return defaultPrice() * 2;
+        }
+        return defaultPrice();
+    }
+}
